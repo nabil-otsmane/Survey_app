e0f13efe940a570ba2b219cfcbecc28c91be1f65
nabil-otsmane <hn_otsmane@esi.dz>
2021-12-08 01:45:13 +0100

    added interactions and indicators

--- front/src/components/surveyCreator.js
+++ front/src/components/surveyCreator.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { MdClose, MdArrowDownward, MdArrowUpward, MdDelete } from 'react-icons/md'
+import { AiOutlineLoading3Quarters } from 'react-icons/ai'
 import { AlertContext, Alert } from '../context'
 
 function Creator({ setOpen }) {
@@ -7,8 +8,24 @@ function Creator({ setOpen }) {
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
     const [questions, setQuestions] = useState([])
+    const [submitting, setSubmitting] = useState(false)
 
     async function submit(showAlert) {
+
+        if (title === "") { 
+            showAlert(Alert.error, "please enter a title.")
+            return
+        }
+
+        if (description === "") {
+            showAlert(Alert.error, "please enter a description.")
+            return
+        } 
+        if (questions.length === 0) {
+            showAlert(Alert.error, "please add at least one question.")
+            return
+        }
+
         let body = JSON.stringify({
             title,
             description,
@@ -20,6 +37,8 @@ function Creator({ setOpen }) {
             headers: { 'Content-Type': 'application/json' },
             body
         };
+
+        setSubmitting(true)
         
         let res = await fetch(`http://127.0.0.1:8000/api/survey`, requestOptions)
 
@@ -28,14 +47,13 @@ function Creator({ setOpen }) {
             showAlert(Alert.error, "Couldn't submit answer.")
 
             console.log("error while creating survey.")
-            // setSubmitting(false);
+            setSubmitting(false);
         } else {
             if (res.status === 200) {
                 showAlert(Alert.success, "submission recorded!")
                 setOpen(false)
                 console.log("success")
-                // setSubmitting(false);
-                // setActive(-1);
+                setSubmitting(false);
             }
         }
     }
@@ -88,6 +106,7 @@ function Creator({ setOpen }) {
                     <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                         <button type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
                                 onClick={() => {submit(showAlert)}}>
+                            {submitting && <div className="p-1 mr-2 animate-spin"><AiOutlineLoading3Quarters /></div>}
                             Create
                         </button>
                         <button type="button" className="mt-3 w-full inline-flex justify-center rounded-md border-none focus:border-none focus-within:border-none px-4 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
